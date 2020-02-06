0216befd543ae653e91ec4bd8ee2a0679d428fea
Veronica <veronicapc1992@gmail.com>
2020-02-06 15:52:15 +0000

    adding starting project

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+	return(
+		<div>
+			<div className='f3'>
+				{'Vero, your current rank is:'}
+			</div>
+			<div className='f1'>
+				{'#5'}
+			</div>
+		</div>
+
+	)
+}
+
+export default Rank;
