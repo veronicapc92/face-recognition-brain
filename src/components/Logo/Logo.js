5af44c9357feaee730a8f1fb20bbd2022988a95c
Veronica <veronicapc1992@gmail.com>
2020-10-06 17:12:41 +0100

    Remove Logo.css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Tilt from 'react-tilt';
-import brain from './brain.png';
-import './Logo.css';
+import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
 
 const Logo = () => {
-	return (
-		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner pa3">
- 					<img style={{paddingTop:'9px'}} src={brain} alt='logo'/>
- 				</div>
-			</Tilt>
-		</div>
-	);
-}
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 30 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingTop: "9px" }} src={brain} alt="logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
 
 export default Logo;
